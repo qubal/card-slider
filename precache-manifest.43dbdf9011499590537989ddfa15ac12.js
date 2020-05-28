13cf73336dccb94502d26bc5069938d7417b8545
Valentin Kazancev <qubalmail@yandex.ru>
2020-05-28 21:59:17 +0700

    Updates

---
+++ precache-manifest.43dbdf9011499590537989ddfa15ac12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b88d7883548e98eef467f8826e4ba399",
+    "url": "/card-slider/index.html"
+  },
+  {
+    "revision": "4ab07a8a5978524f7c04",
+    "url": "/card-slider/static/css/main.72927edc.chunk.css"
+  },
+  {
+    "revision": "96f3a737e6500a3e3107",
+    "url": "/card-slider/static/js/2.135cff5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/card-slider/static/js/2.135cff5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ab07a8a5978524f7c04",
+    "url": "/card-slider/static/js/main.3951484a.chunk.js"
+  },
+  {
+    "revision": "b2b003e32aee4ee3ffb8",
+    "url": "/card-slider/static/js/runtime-main.ee032aef.js"
+  }
+]);
